aba874a0de2a60504d240cc583fee5d766b43f3d
dominguesgabe <gabrieldomingues522@gmail.com>
2023-09-05 23:52:19 -0300

    created main component and added bg in public folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ffb0425669d34ea0"; }
+/******/ 		__webpack_require__.h = function() { return "759fea5eaa49ebb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
