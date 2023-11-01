98c7ec448d36a3ae12e51a628d57ff59bb886e7b
dominguesgabe <gabrieldomingues522@gmail.com>
2023-11-01 11:04:07 -0300

    importing supabase on the repository

--- src/server/repository/todo.ts
+++ src/server/repository/todo.ts
@@ -1,15 +1,6 @@
+import { supabase } from "@server/infra/db/supabase";
 import { HttpNotFoundError } from "@server/infra/errors";
 import { Todo, TodoSchema } from "@server/schema/todo";
-
-//====supabase====
-
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseKey = process.env.SUPABASE_SECRET_KEY || "";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-//==========
 
 interface TodoRepositoryGetParams {
   page?: number;
